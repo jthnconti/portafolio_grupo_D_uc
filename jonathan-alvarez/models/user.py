ae22348440184b86bc351e302bfce267a0643710
jthnconti <jthnconti@users.noreply.github.com>
2025-05-11 01:58:21 +0000

    deploy: cdf30fc554895f7267e12b06992667d7dc7a0c72

--- jonathan-alvarez/models/user.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, username, email):
-        if '@' not in email:
-            raise ValueError("Correo inválido.")
-        self.username = username
-        self.email = email
-
-    def to_dict(self):
-        return {"username": self.username, "email": self.email}
